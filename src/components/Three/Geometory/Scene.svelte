faf8f8d9284a87cd66175d80c66112afeb1b99ac
Yamanaka Tomohiro <yamanaka@un-t.com>
2025-01-28 12:17:41 +0900

    geometoryのアップデート

--- src/components/Three/Geometory/Scene.svelte
+++ src/components/Three/Geometory/Scene.svelte
@@ -2,14 +2,29 @@
   import ThreeCanvas from '@/components/Three/ThreeCanvas.svelte';
   import Items from './Items.svelte';
   import Camera from '../Camera/Camera.svelte';
+  import { Pane, Button, Color, Checkbox } from 'svelte-tweakpane-ui';
+  import { GeometorySettings, grid } from './geometory.svelte.ts';
 
   let controls: any;
-  const grid = 2.8;
   const position = [0, grid * 0.5, 50];
   const target = [0, grid * 0.5, 0];
 </script>
 
 <ThreeCanvas>
+  <Pane title="Control" position="fixed" y={10}>
+    <Button
+      title="Reset Camera"
+      on:click={() => {
+        controls.setTarget(...target, true);
+        controls.setPosition(...position, true);
+      }}
+    />
+    <Color bind:value={GeometorySettings.baseColor} label="Base Color" />
+    <Color bind:value={GeometorySettings.edgeColor} label="Edge Color" />
+    <Checkbox bind:value={GeometorySettings.isNormalMaterial} label="Normal Material" />
+    <Checkbox bind:value={GeometorySettings.isEdge} label="Edge" />
+    <Checkbox bind:value={GeometorySettings.isRotation} label="Rotation" />
+  </Pane>
   <Items />
   <Camera bind:controls {position} {target} />
 </ThreeCanvas>
--- src/components/Three/Geometory/stores.ts
+++
@@ -1,4 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const baseColor = writable('#cccccc');
-export const edgeColor = writable('#000000');
